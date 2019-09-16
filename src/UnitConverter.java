f3fa9405a775776d8479e5cde20a2246df652601
kid <adam_buczek@wp.pl>
2019-09-16 15:31:12 +0200

    start

---
+++ src/UnitConverter.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class UnitConverter extends JFrame{
+
+    public static void main(String[] args) {
+        JFrame ramka=new JFrame("Unit Converter");
+        ramka.setSize(500,500);
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ramka.show();
+        ramka.setLayout(new BorderLayout());
+    }
+
+}
